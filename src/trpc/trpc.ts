61a24c150e45da40a624536d8978e6888f9af35d
parikshitRoy <parikshitroy00@gmail.com>
2024-01-08 23:41:14 +0530

    feat: Implement authentication middleware only authenticated users will be able to call this procedure

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,6 +1,25 @@
-import { initTRPC } from "@trpc/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { TRPCError, initTRPC } from "@trpc/server";
 
 const t = initTRPC.create();
+
+//! Createing middleware for only Authenticated users
+const middleware = t.middleware;
+const isAuth = middleware(async (opts) => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user.id) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return opts.next({
+    ctx: {
+      userId: user.id,
+      user,
+    },
+  });
+});
 
 export const router = t.router;
 export const publicProcedure = t.procedure; //! publicProcedure --- It creates an api end-point that any one can use it regardless the user is authenticated or not ( You can call it like a public API )
