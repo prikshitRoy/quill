6bbeaa76cc0e78c5a16ff97ad5f696b6b4f6f3c9
parikshitRoy <parikshitroy00@gmail.com>
2024-01-10 04:39:42 +0530

    Adding a progress Bar with shadcn-progress bar

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -6,8 +6,11 @@ import { Button } from "./ui/button";
 
 import Dropzone from "react-dropzone";
 import { Cloud, File } from "lucide-react";
+import { Progress } from "./ui/progress";
 
 const UploadDropzone = () => {
+  const [isUploading, setIsUploading] = useState<boolean>(true);
+
   return (
     <Dropzone
       multiple={false}
@@ -43,6 +46,13 @@ const UploadDropzone = () => {
                   <div className="px-3 py-2 h-full text-sm truncate">
                     {acceptedFiles[0].name}
                   </div>
+                </div>
+              ) : null}
+
+              {/* Loading State */}
+              {isUploading ? (
+                <div className="w-full mt-4 max-w-xs mx-auto">
+                  <Progress value={50} className="h-1 w-full bg-zinc-200" />
                 </div>
               ) : null}
             </label>
