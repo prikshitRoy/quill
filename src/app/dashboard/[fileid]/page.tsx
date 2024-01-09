2e6a81c0ab1718ae08531bc1e161608d11f3bc34
parikshitRoy <parikshitroy00@gmail.com>
2024-01-09 22:27:46 +0530

    Making sure the user is signed in. If not, redirect to sign up

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,14 +1,20 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
 interface PageProps {
   params: {
     fileid: string;
   };
 }
 
-const Page = ({ params }: PageProps) => {
+const Page = async ({ params }: PageProps) => {
   // retrieve the file id
   const { fileid } = params;
 
-  // make database call
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileid}`);
 
   return <div>{fileid}</div>;
 };
