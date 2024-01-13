a939d7324b03f50533e90dba3d7cc0e2822e10a3
parikshitRoy <parikshitroy00@gmail.com>
2024-01-14 01:41:01 +0530

    fix: removed @react-pdf/renderer ,build: added react-pdf

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,5 +1,22 @@
-const PdfRenderer = () => {
-  return <div>PdfRenderer Page!</div>;
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+interface PdfRendererProps {
+  url: string;
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">Top bar</div>
+      </div>
+
+      <div className="flex-1 w-full max-h-full">
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default PdfRenderer;
