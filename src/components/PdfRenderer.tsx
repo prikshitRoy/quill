97185314ecc240bcb8ea20e2d51b7600b5c2a7d4
parikshitRoy <parikshitroy00@gmail.com>
2024-01-14 22:50:01 +0530

    feat: Add PDF pageUp & Down Button > - Using the function onLoadSuccess, we get the total number of pages. > - The pageNumber prop of the Page component is set to currPage for dynamic page display.

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { Loader2 } from "lucide-react";
+import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import { useToast } from "./ui/use-toast";
 import { useResizeDetector } from "react-resize-detector";
+import { Button } from "./ui/button";
+import { useState } from "react";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 interface PdfRendererProps {
@@ -18,10 +20,40 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
   //! Resize PDF
   const { width, ref } = useResizeDetector();
 
+  //! numPages = Total number of Pages, currPage = The current page that is being displayed
+  const [numPages, setNumPages] = useState<number>();
+  const [currPage, setCurrPage] = useState<number>(1);
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-        <div className="flex items-center gap-1.5">Top bar</div>
+        <div className="flex items-center gap-1.5">
+          {/* Top Bar */}
+          <Button
+            disabled={currPage <= 1}
+            onClick={() => {
+              setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1));
+            }}
+            variant="ghost"
+            aria-label="previous page"
+          >
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+
+          <Button
+            disabled={numPages === undefined || currPage === numPages}
+            onClick={() => {
+              setCurrPage((prev) =>
+                /* The ! after numPages is a TypeScript non-null assertion operator, which is a way to tell the compiler “this will not be null or undefined here”. */
+                prev + 1 > numPages! ? numPages! : prev + 1
+              );
+            }}
+            variant="ghost"
+            aria-label="previous page"
+          >
+            <ChevronUp className="h-4 w-4" />
+          </Button>
+        </div>
       </div>
 
       <div className="flex-1 w-full max-h-full">
@@ -39,10 +71,13 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
                 variant: "destructive",
               });
             }}
+            /* When the PDF document is successfully loaded (onLoadSuccess), setNumPages is called with the total number of pages in the document, updating the state.  */
+            onLoadSuccess={({ numPages }) => setNumPages(numPages)}
             file={url}
             className="max-h-full"
           >
-            <Page width={width ? width : 1} pageNumber={1} />
+            {/* pageNumber={currPage}: This specifies the page number to display. currPage is a state variable that keeps track of the current page number. It’s updated when the user navigates between pages. */}
+            <Page width={width ? width : 1} pageNumber={currPage} />
           </Document>
         </div>
       </div>
