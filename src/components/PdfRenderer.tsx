e14baf185115131fe8c20e01c1e772b4c4f0961b
parikshitRoy <parikshitroy00@gmail.com>
2024-01-14 13:48:58 +0530

    feat: Add PDF loading state and error handling - Implement a loading state for PDF with a spinning loader. - Add error handling for PDF loading, showing a toast notification on error.

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
+import { useToast } from "./ui/use-toast";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 interface PdfRendererProps {
@@ -10,6 +12,8 @@ interface PdfRendererProps {
 }
 
 const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
@@ -18,7 +22,22 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
 
       <div className="flex-1 w-full max-h-full">
         <div>
-          <Document file={url} className="max-h-full">
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: "Error loading PDF",
+                description: "Please try again later",
+                variant: "destructive",
+              });
+            }}
+            file={url}
+            className="max-h-full"
+          >
             <Page pageNumber={1} />
           </Document>
         </div>
