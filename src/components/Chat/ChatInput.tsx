c19e0e2d74584eee39c5c186d0d3de1617010943
parikshitRoy <parikshitroy00@gmail.com>
2024-01-18 18:57:52 +0530

    `feat`: Adding basic structure component for Chat

---
+++ src/components/Chat/ChatInput.tsx
@@ -0,0 +1,4 @@
+const ChatInput = () => {
+  return <div>ChatInput</div>;
+};
+export default ChatInput;
--- src/components/ChatWrapper.tsx
+++
@@ -1,5 +0,0 @@
-const ChatWrapper = () => {
-  return <div>ChatWrapper Page!!</div>;
-};
-
-export default ChatWrapper;
