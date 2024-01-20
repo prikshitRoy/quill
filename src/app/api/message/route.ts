039edf282ab5280097646db00318d5440b295c9a
parikshitRoy <parikshitroy00@gmail.com>
2024-01-20 23:25:18 +0530

    `feat`:Implement endpoint for asking a question to a PDF file. It validates the incoming data using the SendMessageValidator from the zod library. - ensures that the user is authorized before processing the request. - Add Message model, update User and File models in Prisma schema. - The message is then stored in the database as a user message associated with the specified PDF file.

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,37 @@
+import { db } from "@/db";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  //! endpoint for asking a question to a PDF file
+
+  const body = await req.json();
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const { id: userId } = user ?? { id: null };
+
+  if (!userId) return new Response("UNAUTHORIZED", { status: 404 });
+
+  const { fileId, message } = SendMessageValidator.parse(body);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId,
+    },
+  });
+
+  if (!file) return new Response("Not Found", { status: 404 });
+
+  await db.message.create({
+    data: {
+      text: message,
+      isUserMessage: true,
+      userId,
+      fileId,
+    },
+  });
+};
